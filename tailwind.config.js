1ab505587cdfe517538af17a0682064f10312360
TedoHarisC <tedoharischandra29@gmail.com>
2024-04-13 13:29:30 +0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "inter": ['inter']
+      },
+      colors: {
+        "purple": "#5D50C6",
+        "pink": "#F85E9F",
+        "orange": "#FF5722",
+        "grey": "#191825",
+        "black": "#191825"
+      }
+    },
+  },
+  plugins: [],
+}
+
